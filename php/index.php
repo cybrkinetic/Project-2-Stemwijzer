29fb02663b3b1a1e18a2ac252327c8a612414e20
Thijs Koot <ps251768@summacollege.nl>
2024-06-04 14:17:05 +0200

    Navbar aangemaakt en bestanden netjes gemaakt

---
+++ php/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="navbar" id="myNavbar">
+  <img id="navbarLogo" src="../img/logo-neutraal-kieslab-lichtblauw.svg"/>
+  <h1 id="navbarKiesLab">Neutraal KiesLab<h1>
+    <div>
+
+      <a href="index.php" class="active">Home</a>
+      <a href="#news">Partijen</a>
+      <a href="#contact">Nieuws</a>
+      <a href="#about">Stellingen</a>
+    </div>
+  
+</div>
+
+</body>
+</html>
