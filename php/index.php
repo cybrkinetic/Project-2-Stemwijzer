9ff394a4730f31a8f3ea542d70f7b8b732c485ff
cybrkinetic <ps252155@summacollege.nl>
2024-06-05 14:02:02 +0200

    Index Pagina af

--- php/index.php
+++ php/index.php
@@ -4,24 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Neuraal KiesLab Stemwijzer</title>
 </head>
 <body>
-<div class="navbar" id="myNavbar">
-  <img id="navbarLogo" src="../img/logo-neutraal-kieslab-lichtblauw.svg"/>
-  <div class="h1">
-    <h1 id="navbarKiesLab">Neutraal KiesLab<h1>
+    <header class="header-flex">
+      <a id="kieslab" href="index.php">
+        <img id="kieslablogo" src="../img/logo-neutraal-kieslab-lichtblauw.svg" alt="Kieslab Logo">
+      </a>
+      <a href="index.php" id="stemwijzer">NEUTRAAL KIESLAB</a>
+        <div class="navbar" id="navbar-place">
+          <a href="stellingen.php">STELLINGEN</a>
+          <a href="partijen.php">PARTIJEN</a>
+          <a href="nieuws.php">NIEUWSBERICHTEN</a>
+        </div>
+    </header>
 
-  </div>
-    <div>
+<article class="article-main">
 
-      <a href="index.php" class="active">Home</a>
-      <a href="#news">Partijen</a>
-      <a href="#contact">Nieuws</a>
-      <a href="#about">Stellingen</a>
-    </div>
-  
+<img id="tegels" src="../img/logo-tegels-achtergrond.svg">
+<img id="tegels2" src="../img/logo-tegels-achtergrond.svg">
+<div class="block">
+  <a id="startBtn" href="#">START</a>
+  <p id="informatie">De stemwijzer van het Neutraal Kieslab.</p>
 </div>
+</article>
 
 </body>
 </html>
