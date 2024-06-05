b383d13ea01f69e5ff17f0be26d3c0de7b945261
Thijs Koot <ps251768@summacollege.nl>
2024-06-05 13:09:06 +0200

    navbar af en toegevoegd aan alle pagina's

---
+++ php/header.php
@@ -0,0 +1,10 @@
+<div class="navbar" id="myNavbar">   
+    <a href="../php/index.php"><img id="navbarLogo" src="../img/logo-neutraal-kieslab-lichtblauw.svg"></img></a>
+    <div class="navbarKieslab">Neutraal KiesLab</div>
+    <div>
+        <a class="nav_a" href="../php/index.php">Home</a>
+        <a class="nav_a" href="../php/partijen.php">Partijen</a>
+        <a class="nav_a" href="../php/nieuws.php">Nieuws</a>
+        <a class="nav_a" href="../php/stellingen.php">Stellingen</a>
+     </div>
+</div>
--- php/index.php
+++ php/index.php
@@ -4,24 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
-    <title>Document</title>
+    <title>Home</title>
 </head>
 <body>
-<div class="navbar" id="myNavbar">
-  <img id="navbarLogo" src="../img/logo-neutraal-kieslab-lichtblauw.svg"/>
-  <div class="h1">
-    <h1 id="navbarKiesLab">Neutraal KiesLab<h1>
-
-  </div>
-    <div>
-
-      <a href="index.php" class="active">Home</a>
-      <a href="#news">Partijen</a>
-      <a href="#contact">Nieuws</a>
-      <a href="#about">Stellingen</a>
-    </div>
-  
+<div class="container">
+    <?php require "header.php"?>
 </div>
-
 </body>
 </html>
