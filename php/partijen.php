c86706cb0890222f3af72e0fea4a44378f7400a6
motasm <ps171179@summacollege.nl>
2024-06-06 13:14:06 +0200

    css partijen gelinked aan parijen.php

--- php/partijen.php
+++ php/partijen.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/partijen.css">
     <title>Partijen</title>
 </head>
 <body class="container">
-<?php require "header.php"?>
+<?php include "header.php"?>
 </body>
 </html>
