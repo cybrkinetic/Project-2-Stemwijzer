02c2f634bb33152e3320634c46a45021cab68236
cybrkinetic <ps252155@summacollege.nl>
2024-06-12 13:58:51 +0200

    Nieuws en Stemwijzer paginas responsive

--- php/index.php
+++ php/index.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/index.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/x-icon" href="../img/logo-neutraal-kieslab-lichtblauw.svg">
-    <title>Neutraal KiesLab Stemwijzer</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/index.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="../img/logo-neutraal-kieslab-lichtblauw.svg">
+  <title>Neutraal KiesLab Stemwijzer</title>
 </head>
+
 <body class="container">
   <header class="header-flex">
-    <?php require "header.php"?>
+    <?php require "header.php" ?>
   </header>
   <article class="article-main">
-      <div class="block">
-          <p id="intro">Welkom bij de stemwijzer van het Neutraal Kieslab. Wij hebben deze gemaakt om u te helpen uw keuze te maken in de verkiezingen. Klik op de start knop om te beginnen!</p>
-          <a id="startBtn" href="stemwijzer.php">Start</a>
-      </div>
+    <div class="block">
+      <p id="intro">Welkom bij de stemwijzer van het Neutraal Kieslab. Wij hebben deze gemaakt om u te helpen uw keuze te maken in de verkiezingen. Klik op de start knop om te beginnen!</p>
+      <a id="startBtn" href="stemwijzer.php">Start</a>
+    </div>
   </article>
 </body>
+
 </html>
