d1e410bc4872522286db6f5fbe2cfe8574f0d358
motasm <ps171179@summacollege.nl>
2024-06-25 17:44:24 +0200

    tab-logo toegevoegd aan alle pagina's

--- php/beheerder_stelling.php
+++ php/beheerder_stelling.php
@@ -21,6 +21,7 @@ $dbHandler = new dbHandler();
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="../img/logo-neutraal-kieslab-lichtblauw.svg">
     <title>Beheerder</title>
 </head>
 <?php require "header.php"?>
--- php/partijen.php
+++ php/partijen.php
@@ -15,6 +15,7 @@ $dbHandler = new dbHandler();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/partijen.css">
+    <link rel="icon" type="image/x-icon" href="../img/logo-neutraal-kieslab-lichtblauw.svg">
     <title>Partijen</title>
 </head>
 
